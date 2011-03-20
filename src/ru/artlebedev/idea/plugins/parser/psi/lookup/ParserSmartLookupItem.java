c89a5ab931beb68f9ebbccd2bca3ac5f8aa9ffed
Valeriy Yatsko <dwr@design.ru>
2011-03-20 05:09:02 +0300

    Another doze of structure prettifying (missing files)

---
+++ src/ru/artlebedev/idea/plugins/parser/psi/lookup/ParserSmartLookupItem.java
@@ -0,0 +1,105 @@
+package ru.artlebedev.idea.plugins.parser.psi.lookup;
+
+import com.intellij.codeInsight.lookup.DeferredUserLookupValue;
+import com.intellij.codeInsight.lookup.LookupItem;
+import com.intellij.codeInsight.lookup.LookupValueWithPriority;
+import com.intellij.codeInsight.lookup.LookupValueWithPsiElement;
+import com.intellij.codeInsight.lookup.LookupValueWithUIHint;
+import com.intellij.codeInsight.lookup.PresentableLookupValue;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Iconable;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiNamedElement;
+import com.intellij.ui.RowIcon;
+import ru.artlebedev.idea.plugins.parser.ParserIcons;
+import ru.artlebedev.idea.plugins.parser.psi.api.IsTyped;
+import ru.artlebedev.idea.plugins.parser.psi.api.ParserClass;
+import ru.artlebedev.idea.plugins.parser.psi.api.ParserMethod;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Copyright 2011 Valeriy Yatsko <dwr@design.ru>
+ * Copyright 2006 Jay Bird <a4blank@yahoo.com>
+ * Copyright 2006-2011 ArtLebedev Studio
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class ParserSmartLookupItem implements PresentableLookupValue, LookupValueWithPriority, DeferredUserLookupValue, LookupValueWithUIHint, LookupValueWithPsiElement, Iconable {
+  private PsiNamedElement element;
+  private Icon icon;
+
+  public ParserSmartLookupItem(PsiNamedElement element) {
+    super();
+    this.element = element;
+  }
+
+  public String getPresentation() {
+    return element.getName() == null ? "null" : element.getName();
+  }
+
+  public int getPriority() {
+    return NORMAL;
+  }
+
+  public boolean handleUserSelection(LookupItem lookupItem, Project project) {
+    return true;
+  }
+
+  public String getTypeHint() {
+
+    if (element instanceof ParserMethod) {
+      ParserMethod method = (ParserMethod) element;
+      ParserClass returnValueType = method.getReturnValueType();
+      if (returnValueType != null)
+        return returnValueType.getName();
+    }
+    if (element instanceof IsTyped) {
+      ParserClass aClass = ((IsTyped) element).getType();
+      if (aClass != null)
+        return aClass.getName();
+    }
+    if (element instanceof ParserClass) {
+      return element.getContainingFile().getName();
+    }
+
+    return null;
+  }
+
+  public Color getColorHint() {
+    return null;
+  }
+
+  public boolean isBold() {
+//		if (!element.getContainingFile().isPhysical())
+//			return true;
+    return false;
+  }
+
+  public PsiElement getElement() {
+    return element;
+  }
+
+  public Icon getIcon(int flags) {
+    if (icon == null) {
+      RowIcon i = new RowIcon(2);
+      i.setIcon(element.getIcon(flags), 0);
+      i.setIcon(ParserIcons.EMPTY, 1);
+      icon = i;
+    }
+    return icon;
+  }
+}
+
---
+++ src/ru/artlebedev/idea/plugins/parser/psi/resolve/ParserResolveUtil.java
@@ -0,0 +1,136 @@
+package ru.artlebedev.idea.plugins.parser.psi.resolve;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiElementFilter;
+import com.intellij.psi.util.PsiTreeUtil;
+import ru.artlebedev.idea.plugins.parser.psi.api.ParserCallingReference;
+import ru.artlebedev.idea.plugins.parser.psi.api.ParserClass;
+import ru.artlebedev.idea.plugins.parser.psi.ParserFile;
+import ru.artlebedev.idea.plugins.parser.psi.api.ParserMethod;
+import ru.artlebedev.idea.plugins.parser.psi.api.ParserObject;
+import ru.artlebedev.idea.plugins.parser.psi.api.ParserParameterList;
+import ru.artlebedev.idea.plugins.parser.psi.impl.ParserIncludePathImpl;
+import ru.artlebedev.idea.plugins.parser.psi.impl.ParserMethodReferenceImpl;
+import ru.artlebedev.idea.plugins.parser.psi.ParserPsiUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Copyright 2011 Valeriy Yatsko <dwr@design.ru>
+ * Copyright 2006 Jay Bird <a4blank@yahoo.com>
+ * Copyright 2006-2011 ArtLebedev Studio
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class ParserResolveUtil {
+
+  public static List<ParserMethodReferenceImpl> getIncludesViaUse(PsiFile parserFile) {
+    List<ParserMethodReferenceImpl> result = new ArrayList<ParserMethodReferenceImpl>();
+
+    final PsiElement[] methodElements = PsiTreeUtil.collectElements(parserFile, new PsiElementFilter() {
+      public boolean isAccepted(PsiElement element) {
+        return element instanceof ParserCallingReference;
+      }
+    });
+
+    for (PsiElement methodElement : methodElements) {
+      ParserCallingReference reference = (ParserCallingReference) methodElement;
+      if (reference.getReferenceClass() == null && reference.getReferenceObjects().length == 0) {
+        final ParserMethodReferenceImpl method = (ParserMethodReferenceImpl) reference.getReferenceMethod();
+        if (method != null && method.getName().equals("use")) {
+          result.add(method);
+        }
+      }
+    }
+
+    return result;
+  }
+
+  public static List<ParserObject> collectObjectDeclarationsInElement(PsiElement parent) {
+    return ParserPsiUtil.collectElementsAsList(parent, ParserObject.class);
+  }
+
+  public static List<PsiElement> collectObjectDeclarations(PsiElement element) {
+    List<PsiElement> result = new ArrayList<PsiElement>();
+    PsiElement currentElement = element;
+
+    while (!(currentElement instanceof ParserFile)) {
+      do {
+        if (currentElement instanceof ParserObject)
+          result.add(currentElement);
+        if (currentElement.getPrevSibling() != null)
+          currentElement = currentElement.getPrevSibling();
+      } while (currentElement.getPrevSibling() != null);
+      currentElement = currentElement.getParent();
+    }
+
+    return result;
+
+  }
+
+  public static List<PsiElement> collectClassIncludes(PsiElement element) {
+    List<PsiElement> result = new ArrayList<PsiElement>();
+
+    PsiElement[] includes = PsiTreeUtil.collectElements(element, new PsiElementFilter() {
+      public boolean isAccepted(PsiElement element) {
+        return element instanceof ParserIncludePathImpl;
+      }
+    });
+
+    for (PsiElement include : includes) {
+      ParserIncludePathImpl classIncl = (ParserIncludePathImpl) include;
+      PsiElement parserFile = classIncl.resolve();
+      if (parserFile != null) {
+        ParserClass parserClass = PsiTreeUtil.getChildOfType(parserFile, ParserClass.class);
+        if (parserClass != null)
+          result.add(parserClass);
+      }
+    }
+
+    return result;
+  }
+
+  public static List<ParserMethod> collectMethods(PsiElement where) {
+    return ParserPsiUtil.collectElementsAsList(where, ParserMethod.class);
+  }
+
+  public static List<PsiElement> collectParameters(PsiElement element) {
+    while (!(element instanceof ParserMethod)) {
+      if (element instanceof ParserFile)
+        return new ArrayList<PsiElement>();
+      element = element.getParent();
+    }
+    ParserMethod method = (ParserMethod) element;
+    ParserParameterList parameterList = method.getParameterList();
+    return Arrays.asList(parameterList.getParameters());
+
+  }
+
+  public static Collection<ParserClass> getClassesFromFiles(Collection<ParserFile> parserFiles) {
+    Collection<ParserClass> indexedClasses = new ArrayList<ParserClass>();
+
+    for (ParserFile parserFile : parserFiles) {
+      ParserClass parserClass = PsiTreeUtil.getChildOfType(parserFile, ParserClass.class);
+      if (parserClass != null) {
+        indexedClasses.add(parserClass);
+      }
+    }
+    return indexedClasses;
+  }
+}
+
