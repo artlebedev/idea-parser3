1f6f36513eedc6ba47bf29ab3327095b16cc6d9e
Valeriy Yatsko <dwr@design.ru>
2011-03-26 16:47:08 +0300

    Add copyright to ParserFileViewProvider.java

--- src/ru/artlebedev/idea/plugins/parser/file/ParserFileViewProvider.java
+++ src/ru/artlebedev/idea/plugins/parser/file/ParserFileViewProvider.java
@@ -24,12 +24,25 @@ import java.util.Arrays;
 import java.util.Set;
 
 /**
- * Copyright (c) 2011 Valeriy Yatsko
- * Copyright (c) 2011 ArtLebedev Studio
- * User: dwr
- * Date: 25.03.11
- * Time: 21:07
+ * idea-parser3: slightly useful plugin.
+ * <p/>
+ * Copyright 2011 Valeriy Yatsko <dwr@design.ru>
+ * Copyright 2006 Jay Bird <a4blank@yahoo.com>
+ * Copyright 2006-2011 ArtLebedev Studio
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 public class ParserFileViewProvider extends MultiplePsiFilesPerDocumentFileViewProvider implements TemplateLanguageFileViewProvider {
   private final Language myTemplateDataLanguage;
   private static final THashSet<Language> ourRelevantLanguages =
