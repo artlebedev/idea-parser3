dab704a14aabcb285aae063463b1494fed006f66
Valeriy Yatsko <dwr@design.ru>
2011-04-06 18:22:36 +0400

    Force html extension override in Parser Modules

--- src/ru/artlebedev/idea/plugins/parser/file/ParserFileType.java
+++ src/ru/artlebedev/idea/plugins/parser/file/ParserFileType.java
@@ -51,6 +51,7 @@ public class ParserFileType extends LanguageFileType implements FileTypeIdentifi
   public static final String DEFAULT_EXTENSION = "p";
   public static final String IMPRIMATUR_BLOCK_PROCESSOR_EXTENSION = "bp";
   public static final String IMPRIMATUR_PAGE_PROCESSOR_EXTENSION = "pp";
+  public static final String PARSERED_HTML_EXTENSION = "html";
 
   /**
    * All extensions which are associated with this plugin.
@@ -58,7 +59,8 @@ public class ParserFileType extends LanguageFileType implements FileTypeIdentifi
   public static final String[] extensions = {
           DEFAULT_EXTENSION,
           IMPRIMATUR_BLOCK_PROCESSOR_EXTENSION,
-          IMPRIMATUR_PAGE_PROCESSOR_EXTENSION
+          IMPRIMATUR_PAGE_PROCESSOR_EXTENSION,
+          PARSERED_HTML_EXTENSION
   };
   public static final List<String> extensionList = Arrays.asList(extensions);
 
