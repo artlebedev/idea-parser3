d9d12d1b2db02d1fe1af936deb61ea805c9cb81b
Valeriy Yatsko <dwr@design.ru>
2011-04-07 17:19:39 +0400

    Base64 encode/decode actions (pure Apache 2.0 implementation)

---
+++ src/ru/artlebedev/idea/plugins/parser/actions/Base64DecodeAction.java
@@ -0,0 +1,36 @@
+package ru.artlebedev.idea.plugins.parser.actions;
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * idea-parser3: slightly useful plugin.
+ * <p/>
+ * Copyright 2011 Valeriy Yatsko <dwr@design.ru>
+ * Copyright 2011 ArtLebedev Studio
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class Base64DecodeAction extends AbstractTextReplaceAction {
+  @Override
+  public String transform(String selectedText) {
+    try {
+      byte[] byteArray = Base64.decodeBase64(selectedText.getBytes("UTF-8"));
+      return new String(byteArray, "UTF-8");
+    } catch (UnsupportedEncodingException e) {
+      return null;
+    }
+  }
+}
