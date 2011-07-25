c00f4ebd352b39890cc7e9cb415908a68e1fa092
Valeriy Yatsko <dwr@design.ru>
2011-07-26 03:38:50 +0400

    Add copyright header

--- idea-parser3-help-builder/src/ru/artlebedev/idea/plugins/parser/helpBuilder/ParserDocumentationFileIndexer.java
+++ idea-parser3-help-builder/src/ru/artlebedev/idea/plugins/parser/helpBuilder/ParserDocumentationFileIndexer.java
@@ -11,6 +11,26 @@ import java.io.*;
 import java.util.ArrayList;
 
 /**
+ * idea-parser3: the most advanced parser3 ide.
+ * <p/>
+ * Copyright 2011 <a href="mailto:dwr@design.ru">Valeriy Yatsko</a>
+ * Copyright 2006-2011 ArtLebedev Studio
+ * <p/>
+ * Based on code from www.lucenetutorial.com, which had no copyright header
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
  * This terminal application creates an Apache Lucene index in a folder and adds files into this index
  * based on the input of the user.
  */
