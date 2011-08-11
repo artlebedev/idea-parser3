d091b597ace7a2d3390edc6042a995782af945dc
Valeriy Yatsko <dwr@design.ru>
2011-08-11 23:38:31 +0400

    Initial distribution build toolchain

---
+++ parseride/package_linux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Author: Toader Mihai Claudiu (https://github.com/mtoader)
+# Date: August 7th, 2011
+#
+# Many variables (including source paths) can/must be overriden.
+# Refer to package.sh for the list
+
+TARGET_HOST=linux ./package.sh
